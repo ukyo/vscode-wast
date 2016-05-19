af103ce193d65171c61b01b66dd9eda283dc6a73
ukyo <ukyo.web@gmail.com>
2016-05-19 22:08:02 +0900

    first commit

---
+++ wast/src/server.ts
@@ -0,0 +1,37 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import {
+    createConnection, IConnection,
+    TextDocuments, ITextDocument, Diagnostic,
+    InitializeParams, InitializeResult
+} from 'vscode-languageserver';
+
+// Create a connection for the server. The connection uses Node's IPC as a transport
+let connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+
+// Create a simple text document manager. The text document manager
+// supports full document sync only
+let documents: TextDocuments = new TextDocuments();
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// After the server has started the client sends an initilize request. The server receives
+// in the passed params the rootPath of the workspace plus the client capabilites. 
+let workspaceRoot: string;
+connection.onInitialize((params): InitializeResult => {
+    workspaceRoot = params.rootPath;
+    return {
+        capabilities: {
+            // Tell the client that the server works in FULL text document sync mode
+            textDocumentSync: documents.syncKind
+        }
+    }
+});
+
+// Listen on the connection
+connection.listen();
